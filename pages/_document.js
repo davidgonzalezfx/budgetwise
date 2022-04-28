ed39cb39f2c2074bd5e159c690229209bfd8dae0
davidgonzalez <david.gonzalez.-nd@disney.com>
2022-04-28 08:32:47 -0500

    Fix - Status bar color android

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' href='/icon.png'></link>
-          <meta name='theme-color' content='#22a2f2' />
+          <meta name='theme-color' content='#0d0f12' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <link rel='icon' href='/icon-512x512.png' />
         </Head>
